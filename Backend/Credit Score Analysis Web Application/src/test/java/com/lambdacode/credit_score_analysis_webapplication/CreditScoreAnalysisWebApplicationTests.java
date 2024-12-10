f368046fef892e2f153e6c85634b73ed5a1f7aa9
AMB4609 <amoghbajracharya@gmail.com>
2024-12-10 22:19:02 +0545

    First commit with backend

---
+++ Backend/Credit Score Analysis Web Application/src/test/java/com/lambdacode/credit_score_analysis_webapplication/CreditScoreAnalysisWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdacode.credit_score_analysis_webapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditScoreAnalysisWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
